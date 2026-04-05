d79f1f172728cc31585213e7d112456dafd75f8a
etoxto <ostvan.14@gmail.com>
2026-04-05 12:57:19 +0300

    fix redis

--- src/main/java/com/etoxto/dto/OrderResponse.java
+++ src/main/java/com/etoxto/dto/OrderResponse.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class OrderResponse {
+public class OrderResponse implements Serializable {
     private UUID id;
     private Long customerId;
     private String deliveryAddress;
